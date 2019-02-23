bd2806f6af5d35eb71ce0f18111f753af54c9d00
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-02-23 15:36:26 -0600

    Added initial ios app.

---
+++ ios/HackUTD/HackUTD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackUTD
+//
+//  Created by Kevin J Nguyen on 2/23/19.
+//  Copyright © 2019 Kevin J Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

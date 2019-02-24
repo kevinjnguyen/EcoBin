98bbbda0bf04c5696b6665ac7a1fd865fe1a7a88
Kevin Nguyen <kevinjnguyen2@gmail.com>
2019-02-24 10:34:39 -0600

    Added servo server code.

--- server/server.py
+++ server/server.py
@@ -23,13 +23,47 @@ left_servo = GPIO.PWM(33, 50)
 middle_servo = GPIO.PWM(35, 50)
 right_servo = GPIO.PWM(37, 50)
 
+middle_servo.start(7.3)
+time.sleep(0.5)
+left_servo.start(8)
+time.sleep(0.5)
+right_servo.start(9.25)
+time.sleep(0.5)
+
 class Trasher(utd_pb2_grpc.recycleServicer):
     def TestEcho(self, request, context):
         print(request.message)
         return utd_pb2.Reply(message='Received: ' + request.message )
 
     def MoveTrashDividers(self, request, context):
-        print(request.decision)
+        if (request.decision == 0):
+            left_servo.ChangeDutyCycle(10.5)
+            time.sleep(0.1)
+            middle_servo.ChangeDutyCycle(10)
+            time.sleep(0.1)
+            right_servo.ChangeDutyCycle(5)
+            time.sleep(0.1)
+        elif (request.decision == 1):
+            left_servo.ChangeDutyCycle(8)
+            time.sleep(0.1)
+            middle_servo.ChangeDutyCycle(7.3)
+            time.sleep(0.1)
+            right_servo.ChangeDutyCycle(5)
+            time.sleep(0.1)
+        else:
+            left_servo.ChangeDutyCycle(5)
+            time.sleep(0.1)
+            middle_servo.ChangeDutyCycle(4)
+            time.sleep(0.1)
+            right_servo.ChangeDutyCycle(5)
+            time.sleep(0.1)
+
+        time.sleep(2)
+        left_servo.ChangeDutyCycle(8)
+        time.sleep(0.1)
+        middle_servo.ChangeDutyCycle(7.3)
+        time.sleep(0.1)
+        right_servo.ChangeDutyCycle(9.25)
         return utd_pb2.Reply(message='Received.')
 
 def serve():
